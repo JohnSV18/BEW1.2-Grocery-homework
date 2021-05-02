2007a0d64091e7fdb5ff7c6a7bbb7f8fa2c364e4
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-05-02 08:33:55 -0700

    [Update]-updated code quality for templates and py files

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -5,7 +5,6 @@ from grocery_app.forms import GroceryStoreForm, GroceryItemForm, LoginForm, Sign
 from flask_login import login_required, login_user, logout_user, current_user
 from flask_bcrypt import Bcrypt
 
-# Import app and db from events_app package so that we can run app
 from grocery_app import app, db
 
 main = Blueprint("main", __name__)
@@ -23,13 +22,8 @@ def homepage():
 @main.route('/new_store', methods=['GET', 'POST'])
 @login_required 
 def new_store():
-    # TODO: Create a GroceryStoreForm
     form=GroceryStoreForm()
 
-    # TODO: If form was submitted and was valid:
-    # - create a new GroceryStore object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the store detail page.
     if form.validate_on_submit():
         print("***** trying to print success *****")
         new_store = GroceryStore(
@@ -39,7 +33,6 @@ def new_store():
         db.session.add(new_store)
         db.session.commit()
 
-    # TODO: Send the form to the template and use it to render the form fields
         flash('You have Succesfully created a new store')
         return redirect(url_for('main.store_detail', store_id=new_store.id))
     return render_template('new_store.html', form=form)
@@ -47,14 +40,7 @@ def new_store():
 @main.route('/new_item', methods=['GET','POST'])
 @login_required 
 def new_item():
-    # TODO: Create a GroceryItemForm
     form=GroceryItemForm()
-    # TODO: If form was submitted and was valid:
-    # - create a new GroceryItem object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the item detail page.
-
-    # TODO:  Send the form to the templa te and use it to render the form fields
 
     if form.validate_on_submit():
         print("***** trying to print success *****")
@@ -77,19 +63,13 @@ def new_item():
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
     form = GroceryStoreForm(obj=store)
-    # TODO: Create a GroceryStoreForm and pass in `obj=store`
-
-    # TODO: If form was submitted and was valid:
-    # - update the GroceryStore object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the store detail page.
+    
     if form.validate_on_submit():
         store.title = form.title.data
         store.address = form.address.data
 
         db.session.commit()
 
-    # TODO: Send the form to the template and use it to render the form fields
         flash('You have Succesfully updated your store')
         return redirect(url_for('main.store_detail', store_id=store.id, store=store))
     return render_template('store_detail.html', store=store, form=form)
@@ -113,7 +93,7 @@ def item_detail(item_id):
 
 @main.route('/add_to_shopping_list/<item_id>', methods=['POST'])
 def add_to_shopping_list(item_id):
-    # ... adds item to current_user's shopping list
+  
     item = GroceryItem.query.get(item_id)
     current_user.shopping_list_items.append(item)
     db.session.add(current_user)
@@ -128,13 +108,15 @@ def add_to_shopping_list(item_id):
 @main.route('/shopping_list')
 @login_required
 def shopping_list():
-    # ... get logged in user's shopping list items ...
+   
     shopping_list_items = current_user.shopping_list_items
-    # ... display shopping list items in a template ...
+   
     return render_template('shopping_list.html', shopping_list_items = shopping_list_items)
 
 
-# routes.py
+##########################################
+#            Auth.Routes                 #
+##########################################
 
 auth = Blueprint("auth", __name__)
 
