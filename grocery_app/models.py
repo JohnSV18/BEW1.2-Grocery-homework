c0e4065cfa2f1d3757ac0fe825cb19480b3871cc
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-05-13 12:32:28 -0700

    /

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -37,6 +37,14 @@ class GroceryItem(db.Model):
     store = db.relationship('GroceryStore', back_populates='items')
     user_items = db.relationship('User', secondary='shopping_list', back_populates='shopping_list_items')
 
+class User(db.Model, UserMixin):
+    """User Item"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
+    shopping_list_items = db.relationship('GroceryItem', secondary='shopping_list', back_populates='user_items')
 
 shopping_list_table = db.Table('shopping_list',
     db.Column('grocery_item_id', db.Integer, db.ForeignKey('grocery_item.id')),
